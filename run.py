f77df0898c04a8991ee5bb5a9a9f3a44b4982ee2
demoppal <burmesearchive85@gmail.com>
2026-06-02 07:21:54 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,2 @@
-import run
-import os
-import sys
-
-if __name__ == "__main__":
-    try:
-        # run.so ထဲက start_process function ကို လှမ်းခေါ်ခြင်း
-        run.start_process()
-    except KeyboardInterrupt:
-        print("\n\n \033[1;31m[!] Stopped by user.\033[0m")
-        sys.exit()
-    except Exception as e:
-        print(f"\n\n \033[1;31m[!] Error: {e}\033[0m")
-        
+import c
+c.main()
